69a2f18844b28b43aafbb78116da71b0cd2072ca
WangXu10 <xu.wang@fit2cloud.com>
2024-06-19 18:46:54 +0800

    feat(测试计划): 测试规划关联用例校验

--- backend/services/test-plan/src/test/java/io/metersphere/plan/controller/TestPlanCaseControllerTests.java
+++ backend/services/test-plan/src/test/java/io/metersphere/plan/controller/TestPlanCaseControllerTests.java
@@ -25,7 +25,6 @@ import io.metersphere.system.controller.handler.ResultHolder;
 import io.metersphere.system.domain.User;
 import io.metersphere.system.dto.sdk.SessionUser;
 import io.metersphere.system.dto.user.UserDTO;
-import io.metersphere.system.utils.SessionUtils;
 import jakarta.annotation.Resource;
 import org.apache.commons.collections.CollectionUtils;
 import org.junit.jupiter.api.*;
@@ -372,6 +371,11 @@ public class TestPlanCaseControllerTests extends BaseTest {
         userDTO.setLastOrganizationId("wxx_1234");
         SessionUser user = SessionUser.fromUser(userDTO, sessionId);
         testPlanFunctionalCaseService.associateCollection("plan_1", collectionAssociates, user);
+
+        baseCollectionAssociateRequest.setCollectionId("wxxx1231_1");
+        baseCollectionAssociateRequests.add(baseCollectionAssociateRequest);
+        collectionAssociates.put(AssociateCaseType.FUNCTIONAL, baseCollectionAssociateRequests);
+        Assertions.assertThrows(Exception.class, () -> testPlanFunctionalCaseService.associateCollection("plan_1", collectionAssociates, user));
     }
 
     @Test
